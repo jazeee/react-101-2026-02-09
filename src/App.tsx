3b4b0256aef54a6a07a8685ac237ea73a2f904c5
Jaz Singh <jazeee@gmail.com>
2026-02-09 14:28:49 -0800

    Add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { APP_THEME } from './Mui/theme';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export function App() {
   return (
-    <ThemeProvider theme={APP_THEME}>
-      <CssBaseline />
-      Hello World
-    </ThemeProvider>
+    <ErrorBoundary FallbackComponent={() => <div>Something went wrong</div>}>
+      <ThemeProvider theme={APP_THEME}>
+        <CssBaseline />
+        Hello World
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
