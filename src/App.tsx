13ef7f16b04c010467c0e63187c44d85b99948c9
Jaz Singh <jazeee@gmail.com>
2026-02-09 14:32:24 -0800

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { APP_THEME } from './Mui/theme';
 import { ErrorBoundary } from 'react-error-boundary';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './queryClient';
 
 export function App() {
   return (
     <ErrorBoundary FallbackComponent={() => <div>Something went wrong</div>}>
-      <ThemeProvider theme={APP_THEME}>
-        <CssBaseline />
-        Hello World
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={APP_THEME}>
+          <CssBaseline />
+          Hello World
+        </ThemeProvider>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
